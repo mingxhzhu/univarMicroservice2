14d545a49177b7a4b9d98a51301082b840e8d298
chris benson <cbenson@easyapps.us>
2020-07-24 09:02:42 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,128 @@
+var express = require('express');
+var app = express();
+//var compression = require('compression');
+var MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+//app.use(compression());
+
+app.set('port', (process.env.PORT || 5050));
+app.use(express.json({limit: '50mb'}));
+
+app.use(express.json());
+app.use(express.urlencoded());
+//app.use(app.router);
+
+
+// variables, move to envt file
+var url = 'mongodb://user90bd44:821bc8LIkumxQc01be9@cluster-pgrs1001-0-us-east-1-scalabledbs.cloudstrap.io:29001,cluster-pgrs1001-1-us-east-1-scalabledbs.cloudstrap.io:29001,cluster-pgrs1001-2-us-east-1-scalabledbs.cloudstrap.io:29001/pg-app-2-us-u8e29vmtak05z6erwl2vcuhukq5trh?replicaSet=pgrs1001&ssl=true';
+// Database Name
+const dbName = 'pg-app-2-us-u8e29vmtak05z6erwl2vcuhukq5trh';
+
+// development only
+if ('development' == app.get('env')) {
+  //app.use(express.errorHandler());
+}
+
+
+app.get('/', (req, res) => {
+    res.send('hey, whats up!');
+});
+
+app.post('/', (req, res) => {
+    res.send('were watching!');
+});
+
+
+var customerListMicroservice = function()
+{
+  MongoClient.connect(url, function(err, client) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+
+    const db = client.db(dbName);
+
+    findCustomers(db, function() {
+        client.close();
+      });
+
+    client.close();
+  });
+
+}
+
+var findCustomers = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('Customer');
+  // Find some documents
+  collection.find({}).toArray(function(err, docs) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(docs)
+    callback(docs);
+  });
+}
+
+async function customerList() {
+    const client = await MongoClient.connect(url, { useNewUrlParser: true })
+        .catch(err => { console.log(err); });
+    if (!client) {
+        return;
+    }
+    try {
+        const db = client.db(dbName);
+        let collection = db.collection('Customer');
+        let query = {};
+        let res = await collection.find(query).toArray();
+        console.log(res);
+        return res;
+    } catch (err) {
+        console.log(err);
+    } finally {
+        client.close();
+    }
+}
+
+async function customerRead(customerId) {
+    const client = await MongoClient.connect(url, { useNewUrlParser: true })
+        .catch(err => { console.log(err); });
+    if (!client) {
+        return;
+    }
+    try {
+        const db = client.db(dbName);
+        let collection = db.collection('Customer');
+        let query = { 'CustomerId': customerId }
+        let res = await collection.findOne(query);
+        console.log(res);
+        return res;
+    } catch (err) {
+        console.log(err);
+    } finally {
+        client.close();
+    }
+}
+
+app.get('/CustomerList', async function(req, res) {
+
+  // Use connect method to connect to the server
+  let results = await customerList();
+  res.send(results);
+
+});
+
+
+app.get('/CustomerRead', async function(req, res) {
+
+  var customerId = req.query.customer; //no longer needed, as we get all materials
+  console.log('customer id is', customerId);
+  // Use connect method to connect to the server
+  let results = await customerRead(customerId);
+  res.send(results);
+
+});
+
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
