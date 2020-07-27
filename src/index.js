fe138c1f801ad7d5d3e6cf02f893e449cfd9fa19
Ming Zhu <ming.xh.zhu@gmail.com>
2020-07-27 10:46:55 -0400

    Boot up validation

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import 'dotenv/config'
 import cors from 'cors'
 import express from 'express'
 import routes from './routes'
-import mongodb from "mongodb";
+import mongodb from 'mongodb'
+import assert from 'assert'
 
 const PORT = process.env.PORT || 8080
 const DB = process.env.MONGO_DB || 'test'
+const MONGO_URI = process.env.MONGO_URI || null
+
+assert.notEqual(MONGO_URI, null)
 
 const app = express()
 app.use(cors())
- 
 app.use('/customer', routes.customer);
 // global error handler
 app.use((error, req, res, next) => {
@@ -18,7 +21,7 @@ app.use((error, req, res, next) => {
 })
 
 const MongoClient = mongodb.MongoClient
-const client = new MongoClient(process.env.MONGO_URI)
+const client = new MongoClient(MONGO_URI)
 client.connect(err => {
     if(err){
         console.log(`Failed to connect to database ... \n ${err}`)
